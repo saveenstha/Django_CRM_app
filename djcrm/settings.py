0f2862cab9711af55c9c337fc36c588280fd865c
Saveen Shrestha <biz.saveen@gmail.com>
2023-04-27 08:08:59 +0545

    Changed configuration settings for vercel

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -27,7 +27,6 @@ environ.Env.read_env()
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
