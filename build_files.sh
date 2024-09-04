4325e0d77b9b3a97b8fa65b748281bdb2cea9bf7
saveenstha <biz.saveen@gmail.com>
2024-09-04 23:15:17 +0545

    minor change

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 ## bash configuration for vercel
 python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py collectstatic --noinput
