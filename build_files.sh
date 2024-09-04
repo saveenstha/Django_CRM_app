fedc88ccc313d1c44b6e72e2bb08671db79a49a3
saveenstha <biz.saveen@gmail.com>
2024-09-04 13:24:24 +0545

    minor typo

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 ## bash configuration for vercel
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
